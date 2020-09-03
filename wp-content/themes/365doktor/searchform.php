4977de9d3f84c748857414c91b065bbf688649d7
mmoskovchuk <max.moskovchuk@gmail.com>
2020-09-03 15:44:14 +0300

    fix

--- wp-content/themes/365doktor/searchform.php
+++
@@ -1,5 +0,0 @@
-<form role="search" method="get" id="searchform" class="header__search-form" action="<?php echo home_url( '/' ) ?>" >
-    <input type="text" placeholder="Пошук" value="<?php echo get_search_query() ?>" name="s" id="s" class="header__search-text" />
-    <input type="submit" id="searchsubmit" class="header__search-submit" value="пошук" />
-    <i class="fas fa-search fa-2x"></i>
-</form>
